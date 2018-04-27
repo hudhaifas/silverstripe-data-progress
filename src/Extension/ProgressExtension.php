d1f6b245049c950c162c9e35329895e7dbd9a20c
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-04-27 16:23:35 -0500

    - Upgrading to SS4 & BS4...   * Fixed the progress bar of completed fields.

--- src/Extension/ProgressExtension.php
+++ src/Extension/ProgressExtension.php
@@ -11,18 +11,50 @@ class ProgressExtension
         extends DataExtension {
 
     public function getProgress() {
-        $fields = $this->owner->config()->get('progress_fields');
-        if (!$fields) {
-            return 100;
+        $rawFields = $this->owner->config()->get('progress_fields');
+        if (!$rawFields) {
+            return;
         }
 
-        $num = count($fields);
-        $labels = $this->fieldLabels();
-        $count = 0;
+        // Merge associative / numeric keys
+        $fields = [];
+        foreach ($rawFields as $key => $value) {
+            if (is_int($key)) {
+                $key = $value;
+            }
+            $fields[$key] = $value;
+        }
+
+        if (!$fields) {
+            return;
+        }
+
+        $total = count($fields);
+        $labels = $this->owner->fieldLabels();
+        $done = 0;
+
+//        // Localize fields (if possible)
+//        foreach ($this->fieldLabels(false) as $name => $label) {
+//            // only attempt to localize if the label definition is the same as the field name.
+//            // this will preserve any custom labels set in the summary_fields configuration
+//            if (isset($fields[$name]) && $name === $fields[$name]) {
+//                $fields[$name] = $label;
+//            }
+//        }
 
         foreach ($fields as $name => $specOrName) {
-            var_dump($name . ' -- ' . $specOrName);
+            if ($this->owner->$name) {
+                $done++;
+            }
         }
+
+        return $this->owner
+                        ->customise([
+                            "Done" => $done,
+                            "Total" => $total,
+                            "Percentage" => ($done / $total) * 100,
+                        ])
+                        ->renderWith('Includes/Progress');
     }
 
 }
--- templates/Includes/Progress.ss
+++ templates/Includes/Progress.ss
@@ -1,16 +1,4 @@
-<div>
-    <table class="table">
-        <tr>
-            <th><%t Urban.NAME 'Name' %></th>
-            <th><%t Urban.START_DATE 'Start Date' %></th>
-            <th><%t Urban.END_DATE 'End Date' %></th>
-        </tr>
-        <% loop HistoricalNames %>
-        <tr>
-            <td>$Name</td>
-            <td><% if StartDate %>$StartDate<% else %>--<% end_if %></td>
-            <td><% if EndDate %>$EndDate<% else %>--<% end_if %></td>
-        </tr>
-        <% end_loop %>
-    </table>
+<!-- Turquoise -->
+<div class="progress" style="height: 5px">
+    <div class="progress-bar bg-info" style="width:{$Percentage}%; height: 5px"></div>
 </div>
