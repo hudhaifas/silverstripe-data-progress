2cd31e9be5c5f64130c60a5c5347e5898db9979f
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-04-28 01:44:28 -0500

    - Upgrading to SS4 & BS4  * Added form for incomplete fields

--- src/Extension/ProgressExtension.php
+++ src/Extension/ProgressExtension.php
@@ -10,7 +10,71 @@ use SilverStripe\ORM\DataExtension;
 class ProgressExtension
         extends DataExtension {
 
+    protected static $cache_progress = [];
+
     public function getProgress() {
+        $cachedProgress = self::cache_name_check($this->owner->ClassName, $this->owner->ID);
+        if (isset($cachedProgress)) {
+            return $cachedProgress;
+        }
+
+        $fields = $this->getImportantFields();
+
+        if (!$fields) {
+            return;
+        }
+
+        $total = count($fields);
+        $done = 0;
+        $incomplete = [];
+        foreach ($fields as $name => $specOrName) {
+            if ($this->owner->$name) {
+                $done++;
+            } else {
+                $incomplete[] = $name;
+            }
+        }
+
+        $result = [
+            "Done" => $done,
+            "Total" => $total,
+            "Percentage" => ($done / $total) * 100,
+            "Incomplete" => $incomplete,
+            "IsCompleted" => ($done == $total),
+        ];
+
+        return self::cache_name_check($this->owner->ClassName, $this->owner->ID, $result);
+    }
+
+    public function getProgressBar() {
+        return $this->owner
+                        ->customise($this->getProgress())
+                        ->renderWith('Includes/Progress');
+    }
+
+    public function getImportantItems() {
+        $items = [];
+
+        foreach ($this->getImportantFields() as $fieldName) {
+            if (!$this->owner->$fieldName) {
+                $fieldObject = $this->owner->dbObject($fieldName)->scaffoldFormField(null);
+
+                // Allow fields to opt-out of scaffolding
+                if (!$fieldObject) {
+                    continue;
+                }
+
+                $fieldObject->setTitle($this->owner->fieldLabel($fieldName));
+                $fieldObject->setValue($this->owner->$fieldName);
+
+                $items[] = $fieldObject;
+            }
+        }
+
+        return $items;
+    }
+
+    private function getImportantFields() {
         $rawFields = $this->owner->config()->get('progress_fields');
         if (!$rawFields) {
             return;
@@ -24,37 +88,23 @@ class ProgressExtension
             }
             $fields[$key] = $value;
         }
+        return $fields;
+    }
 
-        if (!$fields) {
-            return;
+    //////// Cache //////// 
+    public static function cache_name_check($className, $objectID, $result = null) {
+        // This is the name used on the permission cache
+        // converts something like 'CanEditType' to 'canedittype'.
+        $cacheKey = "$className-$objectID";
+
+        if (isset(self::$cache_progress[$cacheKey])) {
+            $cachedValues = self::$cache_progress[$cacheKey];
+            return $cachedValues;
         }
 
-        $total = count($fields);
-        $labels = $this->owner->fieldLabels();
-        $done = 0;
+        self::$cache_progress[$cacheKey] = $result;
 
-//        // Localize fields (if possible)
-//        foreach ($this->fieldLabels(false) as $name => $label) {
-//            // only attempt to localize if the label definition is the same as the field name.
-//            // this will preserve any custom labels set in the summary_fields configuration
-//            if (isset($fields[$name]) && $name === $fields[$name]) {
-//                $fields[$name] = $label;
-//            }
-//        }
-
-        foreach ($fields as $name => $specOrName) {
-            if ($this->owner->$name) {
-                $done++;
-            }
-        }
-
-        return $this->owner
-                        ->customise([
-                            "Done" => $done,
-                            "Total" => $total,
-                            "Percentage" => ($done / $total) * 100,
-                        ])
-                        ->renderWith('Includes/Progress');
+        return self::$cache_progress[$cacheKey];
     }
 
 }
